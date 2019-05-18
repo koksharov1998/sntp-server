4addcba9c2da60cc7b6085e3379545de89ff033d
koksharov1998 <koksharov1998@ya.ru>
2019-05-18 21:51:35 +0500

    wip

--- sntp.py
+++ sntp.py
@@ -1,28 +1,43 @@
 import sys
 import socket
+from time import ctime
 
 import ntplib
 
-correction = int(open('config.txt').read())
+s = socket.socket(socket.AF_INET,  socket.SOCK_DGRAM)
+s.sendto(b'\x1b' + 47 * b'\0', ('ntp2.stratum2.ru', 123))
+p = ntplib.NTPPacket(s.recv(1024))
+print(p)
 
-client = ntplib.NTPClient()
-time = client.request('ntp2.stratum2.ru').tx_time
-#print(time.tx_time)
-new_time = time + correction
-print(new_time)
+print(ctime(p.recv_timestamp))
+
 
 sys.exit()
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+correction = int(open('config.txt').read())
+print(correction)
+
+client = ntplib.NTPClient()
+response = client.request('ntp2.stratum2.ru')
+print(ctime(response.tx_time))
+new_time = response.tx_time + correction
+print(new_time)
+print()
+print(response.recv_timestamp)
+print(ctime(response.recv_timestamp))
+print(response.tx_timestamp)
+print(ctime(response.tx_timestamp))
+response.recv_timestamp += correction
+response.tx_timestamp += correction
+print(ctime(response.tx_time))
+
+
+sys.exit()
+server = socket.socket()
 server.bind(('localhost', 123))
 server.listen(10)
+
+connection, address = server.accept()
+
 while True:
-    cl, das = server.accept()
-    cl.sendall(str(time).encode())
-    cl.close()
-'''
-while True:
-    msg = cl.recv(1024)
-    print(msg)
-    if msg:
-        cl.sendall(str(now).encode())
-        cl.close()'''
+    data = connection.recv(1024)
+    print(data.decode())
