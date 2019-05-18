02c29c9eda7da6af568d2074871ee50ad9c5b320
koksharov1998 <koksharov1998@ya.ru>
2019-05-18 20:36:43 +0500

    wip

--- sntp.py
+++ sntp.py
@@ -1,17 +1,24 @@
-import datetime
+import sys
 import socket
 
-now = datetime.datetime.now()
-print(now)
+import ntplib
 
 correction = int(open('config.txt').read())
 
+client = ntplib.NTPClient()
+time = client.request('ntp2.stratum2.ru').tx_time
+#print(time.tx_time)
+new_time = time + correction
+print(new_time)
+
+sys.exit()
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(('localhost', 123))
 server.listen(10)
-cl, das = server.accept()
-cl.sendall(str(now).encode())
-cl.close()
+while True:
+    cl, das = server.accept()
+    cl.sendall(str(time).encode())
+    cl.close()
 '''
 while True:
     msg = cl.recv(1024)
